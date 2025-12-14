402aca8d406856dc7902ab78286aee592651d47b
phuhh98 <phuhh98@gmail.com>
2025-12-14 20:25:30 +0700

    fix(config): add max output token env

--- app/external_services/googleAI.py
+++ app/external_services/googleAI.py
@@ -2,8 +2,10 @@ import os
 
 from google import genai
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "nonsenseKey")
+GEMINI_API_KEY = os.getenv(key="GEMINI_API_KEY", default="nonsenseKey")
 
-GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME", "nonsenseKey")
+GEMINI_MODEL_NAME = os.getenv(key="GEMINI_MODEL_NAME", default="nonsenseKey")
+
+GEMINI_MAX_OUTPUT_TOKEN = os.getenv(key="GEMINI_MAX_OUTPUT_TOKEN", default=2000)
 
 client = genai.Client(api_key=GEMINI_API_KEY)
--- app/llm/models/google.py
+++ app/llm/models/google.py
@@ -1,10 +1,21 @@
-
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
 from pydantic import SecretStr
 
-from app.external_services.googleAI import GEMINI_API_KEY, GEMINI_MODEL_NAME
+from app.external_services.googleAI import (
+    GEMINI_API_KEY,
+    GEMINI_MAX_OUTPUT_TOKEN,
+    GEMINI_MODEL_NAME,
+)
 
-chatModel = ChatGoogleGenerativeAI(model=GEMINI_MODEL_NAME, api_key=SecretStr(
-    GEMINI_API_KEY), temperature=0, max_tokens=2000)
-generationModel = GoogleGenerativeAI(model=GEMINI_MODEL_NAME, api_key=SecretStr(
-    GEMINI_API_KEY), temperature=0, max_tokens=2000)
+chatModel = ChatGoogleGenerativeAI(
+    model=GEMINI_MODEL_NAME,
+    api_key=SecretStr(GEMINI_API_KEY),
+    temperature=0,
+    max_tokens=GEMINI_MAX_OUTPUT_TOKEN,
+)
+generationModel = GoogleGenerativeAI(
+    model=GEMINI_MODEL_NAME,
+    api_key=SecretStr(GEMINI_API_KEY),
+    temperature=0,
+    max_tokens=GEMINI_MAX_OUTPUT_TOKEN,
+)
